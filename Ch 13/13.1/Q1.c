358d30c39f45fb8e734bad9d8d5531d7da62ef2b
MayanPipaliya <158546199+MayanPipaliya@users.noreply.github.com>
2024-03-27 14:25:13 +0530

    c language

---
+++ Ch 13/13.1/Q1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+main()
+{
+	int n,i;
+	
+	printf("enter array size : ");
+	scanf("%d",&n);
+	int a[n];
+	
+	for(i=0;i<n;i++)
+	{
+		printf("\nenter value a[%d] : ",i);
+		scanf("%d",&a[i]);
+	}
+	
+
+	printf("\n\nlength of array is : %d",n);
+}
